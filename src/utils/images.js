f58029dcd249f6dfa0c2bf3dc2eb1e2faca767dc
Impulsa Joven <hola@impulsajoven.pe>
2026-06-09 11:45:35 -0500

    style: revert to local thematic images, increase background opacity and fix z-index

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,11 +1,28 @@
-export const keywordMap = {
-  scholarship: '1',
-  course: '2',
-  job: '3',
-  volunteer: '4'
+// Usamos un catálogo local garantizado para que las imágenes nunca se rompan y coincidan 100% con la temática
+export const imageMap = {
+  scholarship: [
+    '/images/scholarships_1.png',
+    '/images/scholarships_2.png',
+    '/images/estudiar.jpg'
+  ],
+  course: [
+    '/images/courses_1.png',
+    '/images/courses_2.png',
+    '/images/aprender.jpg'
+  ],
+  job: [
+    '/images/jobs_1.png',
+    '/images/jobs_2.png',
+    '/images/trabajar.jpg'
+  ],
+  volunteer: [
+    '/images/volunteering_1.png',
+    '/images/volunteering_2.png',
+    '/images/voluntariado.jpg'
+  ]
 };
 
 export function getUniqueImage(category, index) {
-  const seedId = keywordMap[category] || '5';
-  return `https://picsum.photos/seed/${seedId}${index + 1}/400/300`;
+  const images = imageMap[category] || imageMap.scholarship;
+  return images[index % images.length];
 }
